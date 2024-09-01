523381ce13b077f4bb73052622c84bd891870548
Prasanthsivaselva <Prasanthsivaselva@prasanth1725>
2024-09-01 15:17:14 +0530

    First commit of local repo

---
+++ src/GitPackage/FirstGit.java
@@ -0,0 +1,15 @@
+package GitPackage;
+
+public class FirstGit {
+	public void add(int a,int b) {
+		System.out.println(a+b);
+		
+	}
+	public static void main(String[] args) {
+		
+		FirstGit git=new FirstGit();
+		git.add(10, 15);
+		
+	}
+
+}
