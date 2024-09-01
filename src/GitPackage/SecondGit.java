791f7db12afe56877f433401af0877f831892025
Prasanthsivaselva <Prasanthsivaselva@prasanth1725>
2024-09-01 16:25:03 +0530

    second git is added...

---
+++ src/GitPackage/SecondGit.java
@@ -0,0 +1,15 @@
+package GitPackage;
+
+public class SecondGit {
+	public void add(int a,int b) {
+		System.out.println(a+b);
+		
+	}
+	public static void main(String[] args) {
+		
+		FirstGit git=new FirstGit();
+		git.add(50, 15);
+		
+	}
+
+}
